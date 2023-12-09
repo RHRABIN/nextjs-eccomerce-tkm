dc7ca7b4b17778ce44bee607d55eec88a727e9ba
engadnanhossain <engr.adnanhossain@gmail.com>
2023-12-09 15:07:09 +0600

    handle loading

---
+++ src/app/account/loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div className='flex items-center justify-center h-screen w-full'>
+            <h1>Loading...</h1>
+        </div>
+    );
+};
+
+export default loading;
