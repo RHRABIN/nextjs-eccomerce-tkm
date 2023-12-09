d2252739b1b06766576f87c2a056d5149727bbc7
engadnanhossain <engr.adnanhossain@gmail.com>
2023-12-09 14:28:14 +0600

    add order model

--- src/clientSideRender/orders/OrderDetailsClient.jsx
+++ src/clientSideRender/orders/OrderDetailsClient.jsx
@@ -13,7 +13,31 @@ const OrderDetailsClient = () => {
                 modalOpen={openModal}
                 setModalOpen={setOpenModal}
             >
+                <div className='border-b pb-2'>
+                    <div>
+                        <h4 className='text-blue-500 text-lg'>Order Id: BKON973630</h4>
+                        <p className='text-sm font-[300]'>Date: 12/9/2023, 12:01:02 PM</p>
+                        <p className='text-sm font-[500]'>Total: 2000</p>
+                    </div>
+                </div>
+
                 <OrdersSingleCard />
+
+                <div className='mt-4 border-t py-4'>
+                    <h3 className='font-[500]'>Order summery</h3>
+                    <span className='text-sm flex items-center justify-between'>
+                        <p>Sub total:</p>
+                        <p>1900 ৳</p>
+                    </span>
+                    <span className='text-sm flex items-center justify-between border-b pb-1'>
+                        <p>Shipping:</p>
+                        <p>100 ৳</p>
+                    </span>
+                    <span className='text-sm flex items-center justify-between py-1'>
+                        <p>Total amount:</p>
+                        <p>2000 ৳</p>
+                    </span>
+                </div>
             </Modal>
         </div>
     );
