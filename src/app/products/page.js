a0f6d2bd05ead542ed465e7e21ee1e7cbd1cd601
engadnanhossain <engr.adnanhossain@gmail.com>
2023-12-16 12:37:01 +0600

    responsive proudct page

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -4,14 +4,12 @@ import React from 'react';
 const ProductsSearchPage = () => {
     return (
         <div className='container mx-auto'>
-            <div className='mx-4 md:mx-0'>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-                    {
-                        Array(9).fill().map((_, idx) =>
-                            <ProductCard key={idx} />
-                        )
-                    }
-                </div>
+            <div className='grid grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    Array(9).fill().map((_, idx) =>
+                        <ProductCard key={idx} />
+                    )
+                }
             </div>
         </div>
     );
