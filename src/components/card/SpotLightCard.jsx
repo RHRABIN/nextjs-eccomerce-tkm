a2ef9812a80c5d21e2f255dbd2e6035384cc6ec8
engadnanhossain <engr.adnanhossain@gmail.com>
2023-12-16 13:17:06 +0600

    dynamic campign card

--- src/components/card/SpotLightCard.jsx
+++ src/components/card/SpotLightCard.jsx
@@ -1,17 +1,18 @@
 import Image from 'next/image';
 import React from 'react';
 import img from '../../../public/assets/s1.webp'
+import Link from 'next/link';
 
 const SpotLightCard = () => {
     return (
-        <div className='cursor-pointer'>
+        <Link href='campaign-products/New Arrivals from haruharu' className='cursor-pointer'>
             <Image className='w-full h-auto' height={720} width={1280} src={img} />
             <div className='flex flex-col items-center p-4'>
                 <h1 className='text-gray-800 font-medium text-xl text-center mb-4'>New Arrivals from haruharu</h1>
                 <p className='text-gray-700 leading-6 line-clamp-3 text-center font-light'>Discover haruharu's fermented black rice line, a revolutionary skincare innovation. Harnessing the power of nature's finest ingredients, this collection offers transformative benefits for your skin providing radiance and nourishment.</p>
                 <button className='border border-primary hover:bg-primary text-primary hover:text-white uppercase text-sm py-2 px-6 mt-3'> Shop now</button>
             </div>
-        </div>
+        </Link>
     );
 };
 
