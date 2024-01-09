3b221e576213101ea8cc869c43b760d3cd5d9ef6
engadnanhossain <engr.adnanhossain@gmail.com>
2024-01-09 23:09:45 +0600

    add search params

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,15 +1,30 @@
+'use client'
 import ProductCard from '@/components/card/ProductCard';
 import { getAllProductsBySearch } from '@/config/productsApi';
+import { useSearchParams } from 'next/navigation';
 import React from 'react';
 
-const ProductsSearchPage = async() => {
-    const products = await getAllProductsBySearch('?category=best');
+const ProductsSearchPage = async () => {
+    const searchParams = useSearchParams();
+
+    let products;
+    const category = searchParams.get('category');
+    const search = searchParams.get('search');
+    if (category) {
+        products = await getAllProductsBySearch(`?category=${category}`);
+    } else if (search) {
+        products = await getAllProductsBySearch(`?search=${search}`);
+    }
+
     return (
         <div className='container mx-auto'>
             <div className='grid grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
-                    Array(9).fill().map((_, idx) =>
-                        <ProductCard key={idx} />
+                    products?.data?.result?.data?.map(product =>
+                        <ProductCard
+                            key={product?._id}
+                            product={product}
+                        />
                     )
                 }
             </div>
