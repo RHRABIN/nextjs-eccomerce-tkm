9a71c0cbb7c6338cb5a17eedbf3029660be967c3
engadnanhossain <engr.adnanhossain@gmail.com>
2024-01-20 19:50:27 +0600

    get wishlist product

--- src/app/account/wishlist/page.js
+++ src/app/account/wishlist/page.js
@@ -1,12 +1,24 @@
+'use client'
 import WishlistCard from '@/components/account/wishlist/WishlistCard';
-import React from 'react';
+import { getWishListDataByEmail } from '@/config/wishlistApi';
+import { AuthContext } from '@/context/AuthProvider';
+import React, { useContext } from 'react';
 
-const WishlistPage = () => {
+const WishlistPage = async () => {
+    const { user } = useContext(AuthContext);
+    const { data: wishProudct } = await getWishListDataByEmail(user?.data?.user?.email) || {};
+    console.log('wishlist', wishProudct?.products)
     return (
         <div>
             <h1 className='text-xl text-gray-800 font-[500] pb-4 border-dotted border-b border-b-dark'>My Wishlist</h1>
-            <WishlistCard />
-            <WishlistCard />
+            {
+                wishProudct?.products?.map(product =>
+                    <WishlistCard
+                        key={product?._id}
+                        product={product?.product}
+                    />
+                )
+            }
         </div>
     );
 };
--- src/components/account/wishlist/WishlistCard.jsx
+++ src/components/account/wishlist/WishlistCard.jsx
@@ -1,19 +1,19 @@
 import Image from 'next/image';
 import React from 'react';
-import wishImg from '../../../../public/assets/2.webp'
 import Link from 'next/link';
 import { AiOutlineDelete } from "react-icons/ai";
 
-const WishlistCard = () => {
+const WishlistCard = ({ product }) => {
+    const { name, model, offerPrice, price, images } = product || {};
     return (
         <div className='flex items-center justify-between md:w-3/4 my-2'>
             <div className='flex items-center gap-4'>
-                <Image src={wishImg} height={720} width={1280} className='h-20 w-20 md:h-32 md:w-32' />
+                <Image src={images?.[0]} height={720} width={1280} alt={name} quality={100} className='h-20 w-20 md:h-32 md:w-32' />
 
                 <div>
-                    <Link href='/' className='text-blue-600 text-base md:text-lg font-[300]'>Advanced Snail 92 All In One Cream 100g</Link>
-                    <p className='text-sm font-[300] my-1'>Model: SMC</p>
-                    <p className='text-sm font-[300]'>Price: 1900</p>
+                    <Link href='/' className='text-blue-600 text-base md:text-lg font-[300]'>{name}</Link>
+                    <p className='text-sm font-[300] my-1'>Model: {model}</p>
+                    <p className='text-sm font-[300]'>Price: {offerPrice ? offerPrice : price}</p>
                 </div>
             </div>
 
