7dd2c9ac89be035b0f4a4937335971ea99e47463
engadnanhossain <engr.adnanhossain@gmail.com>
2024-02-05 23:58:37 +0600

    add review section

---
+++ src/components/productDetails/reviewSection/WriteReview.jsx
@@ -0,0 +1,45 @@
+'use client'
+import React, { useState } from 'react';
+
+const WriteReview = ({ setToggleReview }) => {
+    const [rating, setRating] = useState('');
+    const [review, setReview] = useState('');
+    const handleSubmitReview = (e) => {
+        e.preventDefault();
+        // const newReview = {
+        //     rating: rating || '5',
+        //     description: review,
+        //     product: productId
+        // }
+        // singleReview({ data: newReview, userEmail: user?.email, });
+        e.target.reset();
+    }
+    return (
+        <div>
+            <h1 className='font-medium text-gray-800 mt-4'>What would you rate this product?</h1>
+            <form onSubmit={handleSubmitReview}>
+                <div className="rate">
+                    <input onChange={(e) => setRating(e.target.value)} type="radio" id="star5" name="rate" value="5" defaultChecked />
+                    <label htmlFor="star5" title="5">5 stars</label>
+                    <input onChange={(e) => setRating(e.target.value)} type="radio" id="star4" name="rate" value="4" />
+                    <label htmlFor="star4" title="4">4 stars</label>
+                    <input onChange={(e) => setRating(e.target.value)} type="radio" id="star3" name="rate" value="3" />
+                    <label htmlFor="star3" title="3">3 stars</label>
+                    <input onChange={(e) => setRating(e.target.value)} type="radio" id="star2" name="rate" value="2" />
+                    <label htmlFor="star2" title="2">2 stars</label>
+                    <input onChange={(e) => setRating(e.target.value)} type="radio" id="star1" name="rate" value="1" />
+                    <label htmlFor="star1" title="1">1 star</label>
+                </div>
+                <textarea onChange={(e) => setReview(e.target.value)} className='border placeholder:text-sm placeholder:italic border-gray-600 outline-none w-full p-4 my-4' name="" id="" cols="60" rows="3" placeholder='Write a Reivew'></textarea>
+
+                <div className='flex items-center justify-end gap-4 '>
+                    <button onClick={() => setToggleReview(false)} className='border border-black py-2 px-6 mt-2 rounded hover:text-white hover:bg-gray-900 cursor-pointer'>Cancel</button>
+                    <button className='border border-black py-2 px-6 mt-2 rounded hover:text-white hover:bg-gray-900 cursor-pointer'>{'Review'}</button>
+                </div>
+            </form>
+            <h1 className='font-medium text-gray-800'>Tell us your feedback about the product</h1>
+        </div>
+    );
+};
+
+export default WriteReview;
