a1dc93a167f333a72bbb45ed80459c95cc8bb95d
engadnanhossain <engr.adnanhossain@gmail.com>
2024-02-05 23:15:41 +0600

    add generate url param

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -15,7 +15,7 @@ const ProductsSearchPage = async () => {
     } else if (search) {
         products = await getAllProductsBySearch(`?search=${search}`);
     }
-console.log(category)
+
     return (
         <div className='container mx-auto'>
             <div className='grid grid-cols-2 lg:grid-cols-3 gap-5'>
