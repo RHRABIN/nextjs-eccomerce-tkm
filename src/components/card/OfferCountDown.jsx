224275b950b2a8eaa065c908bb01a163ced1f65d
engadnanhossain <engr.adnanhossain@gmail.com>
2024-02-10 12:23:10 +0600

    add offer feature in card

---
+++ src/components/card/OfferCountDown.jsx
@@ -0,0 +1,41 @@
+'use client'
+import React from 'react';
+import Countdown from 'react-countdown';
+
+const OfferCountDown = ({ timeStamps }) => {
+    const renderer = ({ days, hours, minutes, seconds, completed }) => {
+        if (completed) {
+            return (
+                <div className="bg-[hsla(0,0%,80%,.4)] w-3/5 mx-auto rounded-md border border-[#ccc]">
+                    <div className="flex justify-center">
+                        <div className="border-[#ccc]">
+                            <p className="text-xs text-center mx-2">{days}</p>
+                            <p className="text-xs text-center mx-2">
+                                {days > 1 ? 'Days' : 'Day'}
+                            </p>
+                        </div>
+                        <div className="border-l border-[#ccc]">
+                            <p className="text-xs text-center mx-2">{hours}</p>
+                            <p className="text-xs text-center mx-2">
+                                {hours > 1 ? 'Hours' : 'Hour'}
+                            </p>
+                        </div>
+                        <div className="border-l border-[#ccc]">
+                            <p className="text-xs text-center mx-2">{minutes}</p>
+                            <p className="text-xs text-center mx-2">Min</p>
+                        </div>
+                        <div className="border-l border-[#ccc]">
+                            <p className="text-xs text-center mx-2">{seconds}</p>
+                            <p className="text-xs text-center mx-2">Sec</p>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+    };
+    return (
+        <Countdown date={Date.now()} renderer={renderer} />
+    );
+};
+
+export default OfferCountDown;
--- src/components/card/ProductCard.jsx
+++ src/components/card/ProductCard.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { IoIosSearch } from 'react-icons/io';
 import Link from 'next/link';
 import AddToCartClient from '@/clientSideRender/addtoCart/AddToCartClient';
+import OfferCountDown from './OfferCountDown';
 
 const ProductCard = ({ product }) => {
     const { _id, name, images, price, offerPrice, productType, slug, manufacturer } = product || {};
@@ -10,11 +11,16 @@ const ProductCard = ({ product }) => {
         <div className='py-4 relative cursor-pointer group hover:shadow-md hover:shadow-dark hover:scale-105 duration-500 transition'>
             <Link href={`/product-detail/${slug}`}>
                 <Image className='h-44 w-full md:h-auto md:px-10' width={1280} height={720} quality={100} src={images?.length > 0 && images?.[0]} alt='' />
-                <div className='flex flex-col items-center justify-center w-4/5 mx-auto'>
-                    <button className='uppercase text-xs bg-black px-2 py-1 text-white'>{productType}</button>
-                    <p className='uppercase font-semibold my-3'>{manufacturer?.name}</p>
-                    <p className='line-clamp-2 text-sm text-center'>{name}</p>
-                    <p className='text-center font-semibold mt-3 group-hover:mt-5 group-hover:opacity-0'><span className='font-[auto]'>৳</span>{offerPrice ? offerPrice : price}</p>
+                <div className='relative'>
+                    <div className='flex flex-col items-center justify-center w-4/5 mx-auto'>
+                        <button className='uppercase text-xs bg-black px-2 py-1 text-white'>{productType}</button>
+                        <p className='uppercase font-semibold my-3'>{manufacturer?.name}</p>
+                        <p className='line-clamp-2 text-sm text-center'>{name}</p>
+                        <p className='text-center font-semibold mt-3 group-hover:mt-5 group-hover:opacity-0'><span className='font-[auto]'>৳</span>{offerPrice ? offerPrice : price}</p>
+                    </div>
+                    <div className='absolute -top-12 left-0 right-0'>
+                        <OfferCountDown />
+                    </div>
                 </div>
             </Link>
             <div className='flex gap-2 w-full px-2 md:px-5 items-center justify-between mt-5 absolute bottom-3 opacity-0 group-hover:opacity-100'>
