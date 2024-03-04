656ebd1150b085b13d2f00b79999edb96817a883
engadnanhossain <engr.adnanhossain@gmail.com>
2024-03-04 23:27:59 +0600

    fix product qty in product details page

--- src/components/productDetails/AddToCartButton.jsx
+++ src/components/productDetails/AddToCartButton.jsx
@@ -7,12 +7,32 @@ import { FaPlus } from 'react-icons/fa6';
 import { FiMinus } from 'react-icons/fi';
 
 const AddToCartButton = ({ product }) => {
+    const [productQty, setProductQty] = useState(1)
     const { _id, offerPrice } = product || {};
-    const { user } = useContext(AuthContext);
+    const { user, setIsCartSuccess } = useContext(AuthContext);
     const email = user?.data?.user?.email || {};
 
     const [isLoading, setIsLoading] = useState(false);
     const [isSuccess, setIsSuccess] = useState(false);
+
+
+    const handleQtyIncrement = () => {
+        const newQty = productQty + 1;
+        if (productQty >= 5) {
+            toast.error('Already chosen five products');
+        } else {
+            setProductQty(newQty);
+        }
+    }
+
+    const handleQtyDecrement = () => {
+        const newQty = productQty - 1;
+        if (productQty <= 1) {
+            toast.error('Select a minimum of one product');
+        } else {
+            setProductQty(newQty);
+        }
+    }
 
     const handleAddToCart = async () => {
         setIsLoading(true);
@@ -22,12 +42,14 @@ const AddToCartButton = ({ product }) => {
             data = {
                 product: _id,
                 offerPrice,
+                quantity: productQty
             };
         }
 
         try {
             await addToCartNewDataByEmail(email, data) || {};
             setIsSuccess(true);
+            setIsCartSuccess(true)
         } catch (error) {
             console.error('Error adding to cart:', error);
         } finally {
@@ -37,20 +59,20 @@ const AddToCartButton = ({ product }) => {
 
 
     useEffect(() => {
-        if(isSuccess){
+        if (isSuccess) {
             toast.success('Cart addedd Successfully')
         }
-    },[isSuccess])
+    }, [isSuccess])
 
     return (
         <div className='flex items-center gap-4'>
             <div className='w-2/5 border py-2 px-4 md:px-10 flex items-center'>
-                <p className='w-2/5'>1</p>
+                <p className='w-2/5'>{productQty}</p>
                 <div className='w-3/5 flex justify-between'>
-                    <button className='bg-gray-200 p-1 rounded-full' type='button'>
+                    <button onClick={handleQtyDecrement} className='bg-gray-200 p-1 rounded-full' type='button'>
                         <FiMinus />
                     </button>
-                    <button className='bg-gray-200 p-1 rounded-full' type='button'>
+                    <button onClick={handleQtyIncrement} className='bg-gray-200 p-1 rounded-full' type='button'>
                         <FaPlus />
                     </button>
                 </div>
