01a4d7347df5a503eac77e69efe9cf30851f8e53
engadnanhossain <engr.adnanhossain@gmail.com>
2024-03-07 00:28:47 +0600

    change order page

--- src/app/account/orders/page.js
+++ src/app/account/orders/page.js
@@ -2,24 +2,42 @@
 import OrdersCard from '@/components/account/orders/OrdersCard';
 import { getAllOrdersByEmail } from '@/config/addCartToapi';
 import { AuthContext } from '@/context/AuthProvider';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
-const OrdersPage = async () => {
+const OrdersPage = () => {
     const { user } = useContext(AuthContext);
-    const { data: products } = await getAllOrdersByEmail(user?.data?.user?.email) || {};
+    const [products, setProducts] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                if (user?.data?.user?.email) {
+                    const res = await getAllOrdersByEmail(user?.data?.user?.email);
+                    if (res) {
+                        setProducts(res)
+                    }
+                }
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        fetchData()
+    }, [user?.data?.user?.email])
 
     return (
         <div>
             <h1 className='text-xl text-gray-800 font-[500] pb-4 border-dotted border-b border-b-dark'>View Orders</h1>
 
             <div className='mt-4'>
-                {
-                    products?.map(product =>
+                { products?.data?.length > 0 ?
+                    products?.data?.map(product =>
                         <OrdersCard
                             key={product?._id}
                             product={product}
                         />
-                    )
+                    ) : 
+                    <p className='text-center mt-5'>No Orders Found</p>
                 }
             </div>
         </div>
