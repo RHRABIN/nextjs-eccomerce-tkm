345524f2c0b9753c7f6b864bc06893a1dd0fb339
engadnanhossain <engr.adnanhossain@gmail.com>
2024-03-07 23:36:56 +0600

    change mobile accordion

--- src/components/home/skinCare/MobileSkinCare.jsx
+++ src/components/home/skinCare/MobileSkinCare.jsx
@@ -1,5 +1,4 @@
 'use client'
-import AccordionClient from '@/clientSideRender/accordion/AccordionClient';
 import ProductCard from '@/components/card/ProductCard';
 import { productSettings } from '@/utilities/sliderSettings/productSetting';
 import React from 'react';
@@ -7,107 +6,121 @@ import Slider from 'react-slick';
 import Link from 'next/link';
 import Image from 'next/image';
 import img from '../../../../public/assets/category.jpg'
+import { Collapse } from 'antd';
 
 const MobileSkinCare = ({ bestSellingProducts, skinCategory, concernCategory, routineCategory }) => {
     return (
         <div className='mx-4 md:hidden mt-10'>
-            <AccordionClient title={<p className='uppercase' >Best Seller</p>} isDropdown={true}>
-                {/* <Slider {...productSettings} className='mb-20'>
-                    {
-                        bestSellingProducts?.map(product =>
-                            <div key={product?._id} className='px-2 py-10'>
-                                <ProductCard product={product} />
-                            </div>
-                        )
-                    }
-                </Slider> */}
-                <div className='flex overflow-x-scroll details-image gap-2'>
-                    {
-                        bestSellingProducts?.map(product =>
-                            <div key={product?._id} className='px-2 py-10 min-w-[10rem]'>
-                                <ProductCard product={product} />
-                            </div>
-                        )
-                    }
-                </div>
-            </AccordionClient>
 
-            <AccordionClient title={<p className='uppercase' >Skin Type</p>} isDropdown={true}>
-                <Slider {...productSettings} className='mb-20'>
+            <Collapse
+                size="small"
+                expandIconPosition='right'
+                className='border-none mb-2'
+                items={[
                     {
-                        skinCategory?.map(category =>
-                            <div key={category?._id} className='px-2 py-10'>
-                                <Link href={`/products?category=${category?.slug}`} className='relative'>
-                                    <Image
-                                        className='w-full'
-                                        height={720}
-                                        width={1280}
-                                        quality={100}
-                                        src={category?.image ? category?.image : img} alt={category?.title} />
-                                    <p className='absolute bottom-5 text-center w-full'>{category?.title}</p>
-                                </Link>
-                            </div>
-                        )
-                    }
-                </Slider>
-            </AccordionClient>
+                        key: '1',
+                        label: 'BEST SELLER',
+                        children: <div className='flex overflow-x-scroll details-image gap-2'>
+                            {
+                                bestSellingProducts?.map(product =>
+                                    <div key={product?._id} className='px-2 pb-5 min-w-[10rem]'>
+                                        <ProductCard product={product} />
+                                    </div>
+                                )
+                            }
+                        </div>
+                    },
+                ]}
+            />
 
-            <AccordionClient title={<p className='uppercase' >Skin Concern</p>} isDropdown={true}>
-                <Slider {...productSettings} className='mb-20'>
-                    {
-                        concernCategory?.map(category =>
-                            <div key={category?._id} className='px-2 py-10'>
-                                <Link href={`/products?category=${category?.slug}`} className='relative'>
-                                    <Image
-                                        className='w-full'
-                                        height={720}
-                                        width={1280}
-                                        quality={100}
-                                        src={category?.image ? category?.image : img} alt={category?.title} />
-                                    <p className='absolute bottom-5 text-center w-full'>{category?.title}</p>
-                                </Link>
-                            </div>
-                        )
-                    }
-                </Slider>
-            </AccordionClient>
 
-            <AccordionClient title={<p className='uppercase' >Routine</p>} isDropdown={true}>
-                {/* <Slider {...productSettings} className='mb-20'>
+            <Collapse
+                size="small"
+                expandIconPosition='right'
+                className='border-none mb-2'
+                items={[
                     {
-                        routineCategory?.map(category =>
-                            <div key={category?._id} className='px-2 py-10'>
-                                <Link href={`/products?category=${category?.slug}`} className='relative'>
-                                    <Image
-                                        className='w-full'
-                                        height={720}
-                                        width={1280}
-                                        quality={100}
-                                        src={category?.image ? category?.image : img} alt={category?.title} />
-                                    <p className='absolute bottom-5 text-center w-full'>{category?.title}</p>
-                                </Link>
-                            </div>
-                        )
-                    }
-                </Slider> */}
-                <div className='flex overflow-x-scroll details-image gap-2 '>
+                        key: '1',
+                        label: 'SKIN TYPE',
+                        children: <Slider {...productSettings} className='mb-10'>
+                            {
+                                skinCategory?.map(category =>
+                                    <div key={category?._id} className='px-2 pb-5'>
+                                        <Link href={`/products?category=${category?.slug}`} className='relative'>
+                                            <Image
+                                                className='w-full'
+                                                height={720}
+                                                width={1280}
+                                                quality={100}
+                                                src={category?.image ? category?.image : img} alt={category?.title} />
+                                            <p className='absolute bottom-5 text-center w-full'>{category?.title}</p>
+                                        </Link>
+                                    </div>
+                                )
+                            }
+                        </Slider>
+                    },
+                ]}
+            />
+
+            <Collapse
+                size="small"
+                expandIconPosition='right'
+                className='border-none mb-2'
+                items={[
                     {
-                        routineCategory?.map(category =>
-                            <div key={category?._id} className='px-2 py-10 min-w-[10rem]'>
-                                <Link href={`/products?category=${category?.slug}`} className='relative'>
-                                    <Image
-                                        className='w-full'
-                                        height={720}
-                                        width={1280}
-                                        quality={100}
-                                        src={category?.image ? category?.image : img} alt={category?.title} />
-                                    <p className='absolute bottom-5 text-center w-full'>{category?.title}</p>
-                                </Link>
-                            </div>
-                        )
-                    }
-                </div>
-            </AccordionClient>
+                        key: '1',
+                        label: 'SKIN CONCERN',
+                        children: <Slider {...productSettings} className='mb-10'>
+                            {
+                                concernCategory?.map(category =>
+                                    <div key={category?._id} className='px-2 pb-5'>
+                                        <Link href={`/products?category=${category?.slug}`} className='relative'>
+                                            <Image
+                                                className='w-full'
+                                                height={720}
+                                                width={1280}
+                                                quality={100}
+                                                src={category?.image ? category?.image : img} alt={category?.title} />
+                                            <p className='absolute bottom-5 text-center w-full'>{category?.title}</p>
+                                        </Link>
+                                    </div>
+                                )
+                            }
+                        </Slider>
+                    },
+                ]}
+            />
+
+            <Collapse
+                size="small"
+                expandIconPosition='right'
+                className='border-none mb-2'
+                items={[
+                    {
+                        key: '1',
+                        label: 'ROUTINE',
+                        children: <div className='flex overflow-x-scroll details-image gap-2 '>
+                            {
+                                routineCategory?.map(category =>
+                                    <div key={category?._id} className='px-2 min-w-[10rem]'>
+                                        <Link href={`/products?category=${category?.slug}`} className='relative'>
+                                            <Image
+                                                className='w-full'
+                                                height={720}
+                                                width={1280}
+                                                quality={100}
+                                                src={category?.image ? category?.image : img} alt={category?.title} />
+                                            <p className='absolute bottom-5 text-center w-full'>{category?.title}</p>
+                                        </Link>
+                                    </div>
+                                )
+                            }
+                        </div>
+                    },
+                ]}
+            />
+
         </div>
     );
 };
