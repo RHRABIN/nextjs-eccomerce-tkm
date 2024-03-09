7024accfefe610bd19159bd98a44f32058ebdcee
engadnanhossain <engr.adnanhossain@gmail.com>
2024-03-09 20:57:03 +0600

    fix more product issue

--- src/components/productDetails/MoreProducts.jsx
+++ src/components/productDetails/MoreProducts.jsx
@@ -4,21 +4,34 @@ import SliderComponentClient from '@/clientSideRender/sliderComponent/SliderComp
 import { getBrandsProductsByBrandId } from '@/config/productsApi';
 
 const MoreProducts = async ({ brandId, productId }) => {
-    const { data: products } = await getBrandsProductsByBrandId(brandId, productId);
+    const { data: products } = await getBrandsProductsByBrandId(brandId, productId) || {};
 
     return (
         <div className='container mx-auto my-10 md:my-20'>
             <div className='mx-4 md:mx-0'>
                 <h1 className='uppercase text-2xl md:text-3xl font-semibold text-center border-dotted border-b pb-4'>More Products From This Brand</h1>
-                <SliderComponentClient>
-                    {
-                        products?.result?.map(product =>
-                            <div key={product?._id} className='px-2 py-10'>
-                                <ProductCard product={product} />
-                            </div>
-                        )
-                    }
-                </SliderComponentClient>
+                {
+                    products?.result?.length > 4 ?
+                        <SliderComponentClient>
+                            {
+                                products?.result?.map(product =>
+                                    <div key={product?._id} className='px-2 py-10'>
+                                        <ProductCard product={product} />
+                                    </div>
+                                )
+                            }
+                        </SliderComponentClient> :
+                        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+                            {
+                                products?.result?.map(product =>
+                                    <div key={product?._id} className='px-2 py-10'>
+                                        <ProductCard product={product} />
+                                    </div>
+                                )
+                            }
+                        </div>
+                }
+
             </div>
         </div>
     );
