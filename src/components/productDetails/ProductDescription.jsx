199dde9881718e3b1479b7ed87bd2032c7e11bcc
engadnanhossain <engr.adnanhossain@gmail.com>
2024-03-09 21:27:03 +0600

    modified product description page

--- src/components/productDetails/ProductDescription.jsx
+++ src/components/productDetails/ProductDescription.jsx
@@ -1,18 +1,18 @@
 'use client'
 import React from 'react';
-import AccordionClient from '@/clientSideRender/accordion/AccordionClient';
 import DangerHtml from '@/clientSideRender/dangerHtml/DangerHtml';
 import { FaFacebookF, FaInstagram, FaYoutube } from 'react-icons/fa6';
 import AddToCartButton from './AddToCartButton';
 import Link from 'next/link';
 import Image from 'next/image';
 import Rating from '../rating/Rating';
+import { Collapse } from 'antd';
 
 const ProductDescription = ({ product }) => {
     const { name, totalRating, manufacturer, tags, price, offerPrice, description, directions, ingredients, variation, shipping, activities, badge } = product || {};
 
-    
-    
+
+
 
     return (
         <div>
@@ -31,7 +31,7 @@ const ProductDescription = ({ product }) => {
                 <Rating rate={totalRating} />
             </div>
             <div className='text-2xl md:text-3xl font-semibold text-gray-800 flex items-start gap-2 mb-3 border-t border-dotted border-t-dark pt-3'>
-              
+
                 {price == offerPrice ? <> <span className='font-[auto]'>৳</span>{offerPrice ? offerPrice : price}</> : <>
                     <span className='font-[auto]'>৳</span>{offerPrice}
                     <del className='mr-2 text-[24px]'>৳{price}</del>
@@ -70,20 +70,60 @@ const ProductDescription = ({ product }) => {
             }
 
             <div className='mt-2'>
-                <AccordionClient title={'ACTIVITIES'} isDropdown={true}>
-                    <DangerHtml getText={activities} padding={true} />
-                </AccordionClient>
-                <AccordionClient title={'DIRECTIONS'} isDropdown={true}>
-                    <DangerHtml getText={directions} padding={true} />
-                </AccordionClient>
-                <AccordionClient title={'INGREDIENTS'} isDropdown={true}>
-                    <DangerHtml getText={ingredients} padding={true} />
-                </AccordionClient>
-                <AccordionClient title={'SHIPPING'} isDropdown={true}>
+                <Collapse
+                    size="small"
+                    expandIconPosition='right'
+                    className='border-none mb-2 py-1 bg-[#f9f9f9]'
+                    items={[
+                        {
+                            key: '1',
+                            label: <h3 className='font-medium'>ACTIVITIES</h3>,
+                            children: <DangerHtml getText={activities} padding={true} />
+                        },
+                    ]}
+                />
+                <Collapse
+                    size="small"
+                    expandIconPosition='right'
+                    className='border-none mb-2 py-1 bg-[#f9f9f9]'
+                    items={[
+                        {
+                            key: '1',
+                            label: <h3 className='font-medium'>DIRECTIONS</h3>,
+                            children: <DangerHtml getText={directions} padding={true} />
+                        },
+                    ]}
+                />
+                <Collapse
+                    size="small"
+                    expandIconPosition='right'
+                    className='border-none mb-2 py-1 bg-[#f9f9f9]'
+                    items={[
+                        {
+                            key: '1',
+                            label: <h3 className='font-medium'>INGREDIENTS</h3>,
+                            children: <DangerHtml getText={ingredients} padding={true} />
+                        },
+                    ]}
+                />
+                <Collapse
+                    size="small"
+                    expandIconPosition='right'
+                    className='border-none mb-2 py-1 bg-[#f9f9f9]'
+                    items={[
+                        {
+                            key: '1',
+                            label: <h3 className='font-medium'>SHIPPING</h3>,
+                            children: <DangerHtml getText={shipping} padding={true} />
+                        },
+                    ]}
+                />
+
+                {/* <AccordionClient title={'SHIPPING'} isDropdown={true}>
                     <p className='p-2 text-gray-800 font-[300] leading-8 text-sm'>
                         <DangerHtml getText={shipping} padding={true} />
                     </p>
-                </AccordionClient>
+                </AccordionClient> */}
             </div>
         </div>
     );
